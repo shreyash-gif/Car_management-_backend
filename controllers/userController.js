ec145601dd30e6f75568d9253285077b5be501f8
shreyash-gif <shreyashsuradkar123@gmail.com>
2024-11-17 11:25:14 +0530

    Replaced bcrypt with bcryptjs and reinstalled dependencies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,17 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+// Signup Controller
 exports.signup = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
 
+    // Correct usage of bcryptjs
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Create and save the user
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
 
@@ -17,16 +21,22 @@ exports.signup = async (req, res) => {
   }
 };
 
+// Login Controller
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
+    // Find the user by email
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
+    // Compare the input password with the hashed password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
+    // Generate JWT token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
     res.json({ token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
